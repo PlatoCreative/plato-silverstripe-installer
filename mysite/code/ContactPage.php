ba0c4d66a713ad3850cfc3888d109216d7edc24c
Will Dawber <will@platocreative.co.nz>
2015-09-24 10:58:00 +1200

    Update ContactPage.php

--- mysite/code/ContactPage.php
+++ mysite/code/ContactPage.php
@@ -1,6 +1,5 @@
 <?php
-class ContactPage extends UserDefinedForm
-{
+class ContactPage extends UserDefinedForm {
 
     private static $icon = 'mysite/images/contact.png';
 
@@ -11,7 +10,7 @@ class ContactPage extends UserDefinedForm
 
     private static $has_one = array();
 
-    function getCMSFields(){
+    function getCMSFields() {
         $fields = parent::getCMSFields();
         
         // Main Tab
@@ -20,18 +19,18 @@ class ContactPage extends UserDefinedForm
         ), 'Metadata');
         
         // Google Map
-		$fields->addFieldsToTab('Root.GoogleMap', array(
-			TextField::create('GoogleMap', 'Google Map URL')
-		));
+	$fields->addFieldsToTab('Root.GoogleMap', array(
+		TextField::create('GoogleMap', 'Google Map URL')
+	));
         
         return $fields;
     }
     
     // Google Map
-	public function LatLong(){
-		$ll = preg_match('/\@([-.0-9]+),([-.0-9]+)/', $this->GoogleMap, $matches);
-		return $ll ? ArrayData::create(array('Latitude' => $matches[1], 'Longitude' => $matches[2], 'Nice' => $matches[1] . ',' . $matches[2])) : null;
-	}
+    public function LatLong() {
+       $ll = preg_match('/\@([-.0-9]+),([-.0-9]+)/', $this->GoogleMap, $matches);
+       return $ll ? ArrayData::create(array('Latitude' => $matches[1], 'Longitude' => $matches[2], 'Nice' => $matches[1] . ',' . $matches[2])) : null;
+    }
 }
 
 class ContactPage_Controller extends UserDefinedForm_Controller {
