be33b4af0a864ede6abc2bb6ecc897561f4fbe4e
Will Dawber <will@platocreative.co.nz>
2015-09-24 10:56:27 +1200

    Update ContactPage.php

--- mysite/code/ContactPage.php
+++ mysite/code/ContactPage.php
@@ -4,19 +4,37 @@ class ContactPage extends UserDefinedForm
 
     private static $icon = 'mysite/images/contact.png';
 
-    private static $db = array();
+    private static $db = array(
+        'ExtraContent' => 'HTMLText',
+        'GoogleMap' => 'Text'
+    );
 
     private static $has_one = array();
 
-    function getCMSFields()
-    {
+    function getCMSFields(){
         $fields = parent::getCMSFields();
+        
+        // Main Tab
+        $fields->addFieldsToTab('Root.Main', array(
+            HTMLEditorField::create('ExtraContent', 'Extra Content')
+        ), 'Metadata');
+        
+        // Google Map
+		$fields->addFieldsToTab('Root.GoogleMap', array(
+			TextField::create('GoogleMap', 'Google Map URL')
+		));
+        
         return $fields;
     }
+    
+    // Google Map
+	public function LatLong(){
+		$ll = preg_match('/\@([-.0-9]+),([-.0-9]+)/', $this->GoogleMap, $matches);
+		return $ll ? ArrayData::create(array('Latitude' => $matches[1], 'Longitude' => $matches[2], 'Nice' => $matches[1] . ',' . $matches[2])) : null;
+	}
 }
 
-class ContactPage_Controller extends UserDefinedForm_Controller
-{
+class ContactPage_Controller extends UserDefinedForm_Controller {
     private static $allowed_actions = array ();
 
     public function init() {
