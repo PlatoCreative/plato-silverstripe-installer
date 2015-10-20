8f3417ceeee645eb99df7877162b3f039d34624d
Myles Beardsmore <mediabeastnz@gmail.com>
2015-10-20 13:20:51 +1300

    updated base versions
    updated base versions

--- install/PlatoCreative/Install.php
+++ install/PlatoCreative/Install.php
@@ -343,7 +343,7 @@ class Install
                 self::rmdir_recursive("$basePath/themes/default");
             }
             // this will prevent anything from being fired afterwards
-            echo shell_exec('cd ../../ && composer require plato-creative/plato-silverstripe-homeslides:dev-master plato-creative/plato-silverstripe-hometiles:dev-master plato-creative/plato-silverstripe-banners:dev-master plato-creative/plato-silverstripe-gallery:dev-master');
+            echo shell_exec('cd ../../ && composer require plato-creative/plato-silverstripe-homeslides:dev-master plato-creative/plato-silverstripe-hometiles:dev-master plato-creative/plato-silverstripe-banners:1.0 plato-creative/plato-silverstripe-gallery:1.0');
         }
 
       }
