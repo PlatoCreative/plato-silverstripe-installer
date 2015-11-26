41cddd2e244d09b787e562a904133e067432115a
Gorrie <gorriecoe@gmail.com>
2015-11-26 19:34:05 +1300

    Added thirdparty js via bower in vendor

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -23,24 +23,23 @@ class Page_Controller extends ContentController
 
         //Set a custom combined folder under themes so relative paths to images within CSS files don't break
         Requirements::set_combined_files_folder($this->ThemeDir() . '/combined');
-        Requirements::block(THIRDPARTY_DIR . '/jquery/jquery.js');
         Requirements::set_force_js_to_bottom(true);
 
         // [1] use this is this is a custom build
         Requirements::combine_files(
             'app.js',
             array(
-                $this->ThemeDir() . '/js/jquery/dist/jquery.min.js',
-                $this->ThemeDir() . '/foundation/js/foundation.min.js',
-                $this->ThemeDir() . '/js/app.js'
+                'vendor/jquery/dist/jquery.min.js',
+                'vendor/foundation-sites/js/foundation.min.js',
+                'vendor/js/app.js'
             )
         );
 
         if(class_exists('HomeSlide') || class_exists('GalleryItem')) {
-            Requirements::javascript($this->ThemeDir() . '/js/cycle2/jquery.cycle2.min.js');
-            Requirements::javascript($this->ThemeDir() . '/js/cycle2/jquery.cycle2.swipe.min.js');
-            Requirements::javascript($this->ThemeDir() . '/js/fancybox/source/jquery.fancybox.pack.js');
-            Requirements::javascript($this->ThemeDir() . '/js/fancybox/source/helpers/jquery.fancybox-media.js');
+            Requirements::javascript('vendor/cycle2/jquery.cycle2.min.js');
+            Requirements::javascript('vendor/cycle2/jquery.cycle2.swipe.min.js');
+            Requirements::javascript('vendor/fancybox/source/jquery.fancybox.pack.js');
+            Requirements::javascript('vendor/fancybox/source/helpers/jquery.fancybox-media.js');
         }
 
         // Requirements::customScript();
