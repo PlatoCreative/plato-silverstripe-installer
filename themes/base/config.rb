29b98420d71d3cd0c382c7936cdaabe659e61f0a
Gorrie <gorriecoe@gmail.com>
2015-12-21 15:52:52 +1300

    Added auto prefixer to scss

--- themes/base/config.rb
+++ themes/base/config.rb
@@ -1,10 +1,32 @@
-# Extra Requirments
-require 'sass-css-importer'
+# Import scss in folders
 require 'sass-globbing'
+
+# Import css into scss
+require 'sass-css-importer'
+add_import_path Sass::CssImporter::Importer.new("../../thirdparty/")
+
+# Prefixer
+require 'autoprefixer-rails'
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  map = file + '.map'
+
+  if File.exists? map
+    result = AutoprefixerRails.process(css,
+      from: file,
+      to:   file,
+      map:  { prev: File.read(map), inline: false })
+    File.open(file, 'w') { |io| io << result.css }
+    File.open(map,  'w') { |io| io << result.map }
+  else
+    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
+  end
+end
+
+# combines duplicate media queries
 require 'sass-media_query_combiner'
 
 # Require any additional compass plugins here.
-add_import_path Sass::CssImporter::Importer.new("../../thirdparty/")
 add_import_path "../../thirdparty/"
 http_path = "/"
 css_dir = "css"
