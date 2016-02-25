3f1db8b809a30c9f3b8e3b624ba09ef687e87b41
Gorrie <gorriecoe@gmail.com>
2016-02-25 15:23:29 +1300

    Fix conflict issue

--- themes/base/source/js/app.src.js
+++ themes/base/source/js/app.src.js
@@ -1,27 +1,27 @@
 $(document).foundation();
 
-$(document).ready(function() {
-    // google analytics
-    $('[data-ga-label]').each(function(){
-        $(this).on('click', function() {
-            var $trackingName = $(this).data('ga-label'),
-            $tagName = $(this).prop("tagName"),
-            $action = 'click';
-            if($tagName=='input'){
-                $action = 'submit';
+(function($){
+    $(document).ready(function() {
+        // google analytics
+        $('[data-ga-label]').each(function(){
+            $(this).on('click', function() {
+                var $trackingName = $(this).data('ga-label'),
+                $tagName = $(this).prop("tagName"),
+                $action = 'click';
+                if($tagName=='input'){
+                    $action = 'submit';
+                }
+                ga('send', 'event', 'button', $action, $trackingName);
+            });
+        });
+
+        // fancybox
+        $(".fancybox").fancybox({
+            openEffect  : 'none',
+            closeEffect : 'none',
+            helpers : {
+                media : {}
             }
-            ga('send', 'event', 'button', $action, $trackingName);
         });
     });
-
-    // standard images
-    $(".fancybox").fancybox();
-    // videos
-    $('.fancybox-media').fancybox({
-        openEffect  : 'none',
-        closeEffect : 'none',
-        helpers : {
-            media : {}
-        }
-    });
-});
+})(jQuery);
