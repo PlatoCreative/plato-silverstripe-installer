8bde166ba3a97b9faf915500cdd12db271f1fa84
Gorrie <gorriecoe@gmail.com>
2016-03-03 14:05:24 +1300

    Code clean

--- mysite/code/pages/ContactPage.php
+++ mysite/code/pages/ContactPage.php
@@ -1,31 +1,58 @@
 <?php
+/**
+ * Contact Page
+ *
+ * @package silverstripe
+ * @subpackage mysite
+ */
 class ContactPage extends UserDefinedForm
 {
 
     private static $icon = 'mysite/img/contact.png';
 
+    /**
+     * Database fields
+     * @var array
+     */
     private static $db = array(
         'ExtraContent' => 'HTMLText',
         'GoogleMap' => 'Text'
     );
 
+    /**
+     * Many_many relationship
+     * @var array
+     */
     private static $many_many = array(
         'Phones' => 'Phone'
     );
 
+    /**
+     * {@inheritdoc }
+     * @var array
+     */
     private static $many_many_extraFields = array(
         'Phones' => array (
             'Sort' => 'Int'
         )
     );
 
-    public function getCMSFields() {
+    /**
+     * CMS Fields
+     * @return FieldList
+     */
+    public function getCMSFields()
+    {
         $fields = parent::getCMSFields();
 
         // Main Tab
-        $fields->addFieldsToTab('Root.Main', array(
-            HTMLEditorField::create('ExtraContent', 'Extra Content Area')
-        ), 'Metadata');
+        $fields->addFieldsToTab(
+            'Root.Main',
+            array(
+                HTMLEditorField::create('ExtraContent', 'Extra Content Area')
+            ),
+            'Metadata'
+        );
 
         $fields->addFieldsToTab(
             'Root.Phone',
@@ -46,17 +73,20 @@ class ContactPage extends UserDefinedForm
         );
 
         // Google Map
-	    $fields->addFieldsToTab('Root.GoogleMap', array(
-    		TextField::create('GoogleMap', 'Google Map URL')
-    	));
+        $fields->addFieldsToTab(
+            'Root.GoogleMap',
+            array(
+                TextField::create('GoogleMap', 'Google Map URL')
+            )
+        );
 
         return $fields;
     }
 
     // Google Map
     public function LatLong() {
-       $ll = preg_match('/\@([-.0-9]+),([-.0-9]+)/', $this->GoogleMap, $matches);
-       return $ll ? ArrayData::create(array('Latitude' => $matches[1], 'Longitude' => $matches[2], 'Nice' => $matches[1] . ',' . $matches[2])) : null;
+        $ll = preg_match('/\@([-.0-9]+),([-.0-9]+)/', $this->GoogleMap, $matches);
+        return $ll ? ArrayData::create(array('Latitude' => $matches[1], 'Longitude' => $matches[2], 'Nice' => $matches[1] . ',' . $matches[2])) : null;
     }
 }
 
