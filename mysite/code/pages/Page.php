e713e77ebe0a026d53f8e96497b576d875e16ffd
Myles Derham <mediabeastnz@gmail.com>
2016-11-16 12:25:50 +1300

    Added all changes required to make the gulp version of the installer work.

--- mysite/code/pages/Page.php
+++ mysite/code/pages/Page.php
@@ -21,25 +21,8 @@ class Page_Controller extends ContentController
     public function init() {
         parent::init();
 
-        //Set a custom combined folder under themes so relative paths to images within CSS files don't break
-        Requirements::set_combined_files_folder($this->ThemeDir() . '/combined');
         Requirements::set_force_js_to_bottom(true);
-
-        $requiredJS = array(
-            'thirdparty/jquery/dist/jquery.min.js',
-            'thirdparty/foundation-sites/dist/foundation.min.js',
-            'thirdparty/jquery-cycle2/build/jquery.cycle2.min.js',
-            'thirdparty/jquery-cycle2/build/plugin/jquery.cycle2.swipe.min.js'
-        );
-
-        $requiredJS[] = $this->ThemeDir().'/js/app.js';
-
-        // [1] use this is this is a custom build
-        Requirements::combine_files(
-            'app.js',
-            $requiredJS
-        );
-
+        Requirements::javascript($this->ThemeDir().'/js/app.min.js');
 
         // Requirements::customScript();
     }
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    profile: false,
+    stats: {
+        hash: false,
+        version: false,
+        timings: false,
+        assets: false,
+        chunks: false,
+        modules: false,
+        reasons: false,
+        children: false,
+        source: false,
+        errors: false,
+        errorDetails: false,
+        warnings: false,
+        publicPath: false
+    }
+};
