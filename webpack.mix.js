32802c793ef4818ffa3def3d7e12391e92b1dfe3
Myles Derham <mediabeastnz@gmail.com>
2017-06-14 09:15:47 +1200

    updated to include options and clearer layout

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,18 +6,35 @@ const { mix } = require('laravel-mix');
 /*-------------------------------------------------------------------
 Tasks
 -------------------------------------------------------------------*/
+
+// javascript
+mix.js('themes/base/js/app.js', 'themes/base/js/app.min.js');
 mix.autoload({
     'jquery': ['$', 'jQuery', 'window.jQuery'],
     'axios': ['window.axios']
-})
-.js('themes/base/js/app.js', 'themes/base/js/app.min.js').sourceMaps()
-.sass('themes/base/scss/app.scss',  'themes/base/css/').options({
-    processCssUrls: false
-})
-.sass('themes/base/scss/editor.scss', 'themes/base/css/');
-
-// BrowserSync - e.g. "localhost/mysite.com"
+});
+mix.extract(['jquery']);
+// scss
+mix.sass('themes/base/scss/app.scss',  'themes/base/css/');
+mix.sass('themes/base/scss/editor.scss', 'themes/base/css/');
+// generate sourcemaps
+mix.sourceMaps();
+// mix options
+mix.options({
+  processCssUrls: false,
+  //purifyCss: {
+  //    paths: glob.sync([
+  //        path.join(__dirname, 'themes/base/templates/**/*.ss')
+  //    ]),
+  //    minimize: true
+  //},
+  purifyCss: false,
+  postCss: [require('autoprefixer')],
+  clearConsole: true
+});
+// BrowserSync
 mix.browserSync({
     proxy: 'localhost/[YOURSITENAME.CO.NZ]',
-    files: ["themes/base/css/*", "themes/base/js/app.min.js", "themes/base/templates/*.ss", "themes/base/templates/*/*.ss"]
+    files: ["themes/base/css/*", "themes/base/js/app.min.js", "themes/base/templates/**/*.ss"]
 });
+
